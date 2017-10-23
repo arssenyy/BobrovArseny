9b65b64646a381998e4dd80f67d3b3f49e60faa3
arssenyy <32873766+arssenyy@users.noreply.github.com>
2017-10-23 13:47:32 +0300

    Outlets

--- Vkapp/ViewController.swift
+++ Vkapp/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func SignInButtonPush(_ sender: Any) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
